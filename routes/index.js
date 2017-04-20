6ae3155568e7bdd8eabd1ba73da085b519d6b034
Christian Florez <florez.christian@gmail.com>
2017-04-20 14:08:37 -0400

    finish demo

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var router = express.Router();
+const {gods} = require('../models/gods');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { gods: gods });
+});
+
+module.exports = router;
